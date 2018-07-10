9923f6e04de93bb5e239d0bd2920c5a40608ecd9
jessicaparker13 <parkerjessica2001@gmail.com>
2018-07-10 17:59:25 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,13 @@
+# def mothers_day
+ # puts "Happy Mother's Day, Mom!"
+# end 
+
+
+def mothers_day(name)
+  "Happy Mother's Day, #{name}!"
+end
+
+def mothers_day(name = "Mom")
+  "Happy Mother's Day, #{name}!"
+end
+
